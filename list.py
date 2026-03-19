ed7f76fefd057a3d890caab1c95183bca9b4b8e1
Sahil kumawat <viratkumawat137@gmail.com>
2026-03-19 18:28:58 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,48 @@
+# # vowel print kerwane kee leye 
+# a="i live in jaipur"
+# length=len(a)
+# counter = 0
+# while counter < length:
+#     if a [counter] in "ieouaAIEOU":
+#         print(a[counter])
+# counter += 1
+
+
+
+# a="i live in jaipur"
+# length=len(a)
+# counter = 0
+# sum=0
+# while counter < length:
+#     if a [counter] in "ieouaAIEOU":
+#       counter += 1
+# sum +=1
+# print(sum)
+
+
+
+
+# # space count krnee ke leyee 
+# a = "my name is virat and i live in india."
+# print( a.count (" "))
+
+#list
+
+# numbers = [0,1,2,3,4,5,6,7,8,9]
+# print(numbers)
+
+
+# data=[12,12.2,False,"virat",True]
+# print(data)
+
+# #data manipulation in list 
+# data=[12,12.2,False,"virat",True]
+# data[0]=90
+# print(data)
+
+# data=[ 12, 12, False, "virat", True ]
+# print=( data[0] + data[1] )
+
+a=[ 12, 12, False, "virat", True ]
+print(a[3][1])
+
