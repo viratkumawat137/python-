cfc650ec31cd6fc85c8717d6e935096a379e8a9a
Sahil kumawat <viratkumawat137@gmail.com>
2026-03-22 12:01:37 +0530

    Update string_.py

--- string_.py
+++ string_.py
@@ -26,6 +26,120 @@ i="0123456789"
 # print(i [2: 7:2])
 print(i [-9: -2:2])
 
+##lenth kee leyee 
+# i="hello virat "
+# print(len(i))
+
+#membership opraters
+# i="hello virat "
+# print("h" in i)
+
+# i="hello virat "
+# print("f" in i)
+
+# i="hello virat "
+# print(" " in i)
+
+
+
+# #agr kisi world ko aad krna ho tb ==>
+# a="wello"
+# a ="H" + a[ 1 : ]
+# print(a)
+
+
+
+# #small letters kee leye 
+# a="HELLO viRAT"
+# print(a.lower()) #output:hello 
+
+#space ko hatane kee leye (remove)
+# a="  hello sir    "
+# print(a.strip())
+
+
+# test="hii hello virat kumawat "
+# print("not" in test )
+
+
+# my_string= "hello world"
+# print(my_string . find( "O" ) )
+
+# print("hello sir virat kumawat".find( " ",6))
+
+
+# a="i am belong to jaipur "
+# print(a.count("a"))
+# print(a.count(" "))
+
+
+# # ISUPPER
+# a="VIRAT "
+# print(a.isupper())
+
+
+# a="VIRAt12 "
+# print(a.isupper())
+
+# ISlower
+# a="viratt12 "
+# print(a.islower())
+
+
+
+# #isalnum
+# a="jds223rjsf"
+# print(a.isalnum())
+
+# isnumeric
+# a="373947937"
+# print(a.isnumeric())
+
+
+# a="-3-73947937"
+# print(a.isnumeric())
+
+# a="i am virat"
+# print(a.capitalize())
+
+# # title 
+# a="i am virat"
+# print(a.title())
+
+
+# a="This is a test string"
+# print(a[0])
+# print(a[1])
+# print(a[2])
+# print(a[3])
+# print(a[4])
+
+
+# a="INDIA"
+# length=len(a)
+# print(length)
+
+# counter=0
+# while counter < length:
+#     print(a[counter])
+#     counter+=1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
