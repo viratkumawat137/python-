5887d2afacde4613356b011710e7befa6b226d89
Sahil kumawat <viratkumawat137@gmail.com>
2026-03-24 18:43:51 +0530

    Update string_.py

--- string_.py
+++ string_.py
@@ -1,172 +1,108 @@
-# #sliceing
-   
-
-# string="computer"
-# print(string[:7])
-# print(string[2:])
-# print(string[2:6])
-
-
-
-# i="0123456789"
-# print(i[-6:-3])
-# print(i[1:10])
-# print(i[2:3])
-# print(i[-8:-1])
-
-
-
-##start ,end,difference
-# i="0123456789"
-# print(i [: :2])
-# print(i [: :-1])
-# print(i [: :-2])
-# print(i [2: 7:2])
-# print(i [-9: -2:2])
-
-##lenth kee leyee 
-# i="hello virat "
-# print(len(i))
-
-#membership opraters
-# i="hello virat "
-# print("h" in i)
-
-# i="hello virat "
-# print("f" in i)
-
-# i="hello virat "
-# print(" " in i)
-
-
-
-# #agr kisi world ko aad krna ho tb ==>
-# a="wello"
-# a ="H" + a[ 1 : ]
-# print(a)
-
-
-
-# #small letters kee leye 
-# a="HELLO viRAT"
-# print(a.lower()) #output:hello 
-
-#space ko hatane kee leye (remove)
-# a="  hello sir    "
-# print(a.strip())
-
-# my_string="--sahil kumawat--"
-# print(my_string.strip("-"))
-
-
-# my_string="--sahil--kumawat--"
-# print(my_string.strip("-"))
-
-# my_string="--sahil--kumawat***--"
-# print(my_string.strip("-*"))
-
-
-# my_string="--sahil--kumawat***--"
-# print(my_string.lstrip("-"))
-
-# my_string="--sahil--kumawat***--"
-# print(my_string.rstrip("-"))
-
-
-# my_string="sahil--kumawat***--"
-# print(my_string.startswith("sahil"))
-
-
-
-# my_string="sahil--kumawat***--"
-# print(my_string.startswith("ahil"))
-
-
-
-# my_string="sahil--kumawat bye."
-# print(my_string.endswith("bye."))
-
-
-# #justifiy the string
-# print("acbsabs".rjust(100," "))
-
-# print("acbsabs".rjust(10,"+"))
-
-# print("acbsabs".ljust(10,"+"))
-
-
-
-# my_string="sahil--kumawat bye."
-# print(my_string.replace("s", "S"))
-
-
-
-
-# test="hii hello virat kumawat "
-# print("not" in test )
-
-
-# my_string= "hello world"
-# print(my_string . find( "O" ) )
-
-# print("hello sir virat kumawat".find( " ",6))
-
-
-# a="i am belong to jaipur "
-# print(a.count("a"))
-# print(a.count(" "))
-
-
-# # ISUPPER
-# a="VIRAT "
-# print(a.isupper())
-
-
-# a="VIRAt12 "
-# print(a.isupper())
-
-# ISlower
-# a="viratt12 "
-# print(a.islower())
-
-
-
-# #isalnum
-# a="jds223rjsf"
-# print(a.isalnum())
-
-# isnumeric
-# a="373947937"
-# print(a.isnumeric())
-
-
-# a="-3-73947937"
-# print(a.isnumeric())
-
-# a="i am virat"
-# print(a.capitalize())
-
-# # title 
-# a="i am virat"
-# print(a.title())
-
-
-# a="This is a test string"
-# print(a[0])
-# print(a[1])
-# print(a[2])
-# print(a[3])
-# print(a[4])
-
-
-# a="INDIA"
+# # vowel print kerwane kee leye 
+# a="i live in jaipur"
 # length=len(a)
-# print(length)
-
-# counter=0
+# counter = 0
 # while counter < length:
-#     print(a[counter])
-#     counter+=1
+#     if a [counter] in "ieouaAIEOU":
+#         print(a[counter])
+# counter += 1
+
+
+
+# a="i live in jaipur"
+# length=len(a)
+# counter = 0
+# sum=0
+# while counter < length:
+#     if a [counter] in "ieouaAIEOU":
+#       counter += 1
+# sum +=1
+# print(sum)
+
+
+
+
+# # space count krnee ke leyee 
+# a = "my name is virat and i live in india."
+# print( a.count (" "))
+
+#list
+
+# numbers = [0,1,2,3,4,5,6,7,8,9]
+# print(numbers)
+
+
+# data=[12,12.2,False,"virat",True]
+# print(data)
+
+# #data manipulation in list 
+# data=[12,12.2,False,"virat",True]
+# data[0]=90
+# print(data)
+
+# data=[ 12, 12, False, "virat", True ]
+# print=( data[0] + data[1] )
+
+# a=[ 12, 12, False, "virat", True ]
+# print(a[3][1])
+
+
+# a=[[[[137]]]]
+# print(a[0] [0][0][0])
+
+
+# a=["virat",
+#    25,
+#    "jaipur",
+#    303830,
+#    ["test",34,[47689,[635856]]]
+#    ]
+
+# print(a[-1][-1][-1][-1])
+
+# a=["virat",
+#    25,
+#    "jaipur",
+#    303830,
+#    ["test",34,[47689,[635856]]]
+#    ]
+
+# print(a[-1][-1][-1][-1][-1])
+
+
+# refrence verible
+# a=["sdf",1,2,3,4,6,8]
+# b=a
+# print(a,b)
+# b[0] = "virat"
+# print(a,b)
+
+
+
+#sliceing
+# number=[0,1,2,3,4,5,6,7,8,9]
+# print (number[ : ])
+# print ( f"slice [2:6]:{number[2:6]}")
+# print ( f"slice [:6]:{number[:6]}")
+# print ( f"slice [2:]:{number[2:]}")
+# print ( f"slice [-1: :-1]:{number[: :-1]}")
+# print ( f"slice [2:7:2]:{number[2: 7:2]}")
+# print ( f"slice [: :2]:{number[: :2]}")
+# print ( f"slice [-8:-1:2]:{number[-8: -1:2]}")
+
+# homework 
+# numbers = [123, 34, 5, 23, 5, 23, 566, 88, 98]
+# print(numbers[-3: -8 : -2])
+
+
+
+# a=["graps","mango","orange","cherry"]
+# a[1]="date"
+# print(f" total item :{a}")
+
+# print(a[1:3])
+# print(a[1][1:3])
+
 
 
 
