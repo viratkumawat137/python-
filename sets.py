4547510b5d88ef51df53c34532f03c5beb16304e
Sahil kumawat <viratkumawat137@gmail.com>
2026-04-22 20:41:58 +0530

    Add files via upload

---
+++ sets.py
@@ -0,0 +1,111 @@
+#day=16 ==>set
+#creat set  #set dublicate item nhi leta 
+
+# fruits={"apple","mango","banana","graphs"}
+# print(fruits)
+
+
+#aad item
+# fruits.add("lassi")
+# print(fruits)
+
+
+# fruits.add("apple")
+# print(fruits)
+
+
+##Remove items
+# fruits.discard("lassi")
+# print(fruits)
+
+
+#set operations
+# a={1,2,3}
+# b={3,5,4}
+
+# print("union" ,a|b)
+# print("intersection",a&b)
+# print("difrence",a-b)
+# print("difrence",b-a)
+# print("symmetric diffrence ",a^b)
+# print("symmetric diffrence ",b^a)
+
+
+
+# #remove dublicate
+# citis={"jaipur","udaipur","sikar","jaipur"}
+# text=set(citis)
+# print(text)
+
+
+# #mising values
+# list1={"david","munawar","zone","lancho"}
+# list2={"david","munawar","zone"}
+# missing=list1 - list2
+# print(missing)
+
+
+# #comman skills
+# list1={"david","munawar","lancho","carlow"}
+# list2={"david","munawar","zone"}
+# comman=list1 & list2
+# print(comman)
+
+# next topic dicsonary
+
+# a = {1,2,3,4,5}
+# b = {3,7,2,6,4}
+# # c = a.union(b)
+# print(c)
+
+#symattric difrence
+# a = {1,2,3,4,5}
+# b = {3,7,2,6,4}
+# print(a.symmetric_difference(b))
+
+
+
+# a = {1,2,3,4,5}
+# b = {3,7,2,6,4}
+# print(a.intersection(b))
+
+# a={1,2,3}
+# b={1,2,3,4,5}
+# print(a.issubset(b))
+
+
+# a={1,2,3}
+# b={1,2,3,4,5}
+# print(a.issuperset(b))
+# print(b.issuperset(a))
+# print(f" is {a} subset of{b}? {a.issuperset (b) }")
+# print(a.isdisjoint(b))
+
+
+#frozenset
+a=frozenset([1,2,3,4,5])
+b=set([3,4,5,6,7])
+# print(a,b)
+# print(a.pop())
+# print(b.pop())
+# print(dir())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
