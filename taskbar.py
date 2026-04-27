faeaf2d37d66c94b34f494afbad2a6e100edb6f2
Sahil kumawat <viratkumawat137@gmail.com>
2026-04-27 16:51:05 +0530

    Add files via upload

---
+++ taskbar.py
@@ -0,0 +1,287 @@
+###task 1
+# print("hello world")
+
+
+###task 2
+# print("sahil \n 13")
+
+###task 3
+# print(1| 2 | 3 )
+# print(1 ,2 ,3 ,sep="|")
+
+###task 4
+
+# a = 5
+# while a >0:
+#     print(a)
+#     a = a -1
+
+
+# a = 1
+# while a < 6:
+#     print(a)
+#     a = a + 1
+
+
+###task 5
+
+# ====>
+
+# a=input("some word write")
+# b=input("some word write")
+# c=input("some word write")
+# d=input("some word write")
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# d=int(d)
+# print(a+b+c+d)
+
+# ====>
+
+# a=input("some word write")
+# b=input("some word write")
+# c=input("some word write")
+# d=input("some word write")
+# a=int(a)
+# b=int(b)
+# c=int(c)
+# d=int(d)
+# print(a*b*c*d)
+
+
+# # ====>
+# task 5
+
+# n = 4
+# total_sum = 0
+# i = 1  # Ye hamara counter hai
+
+# while i <= n:
+#     total_sum += i  # total_sum mein i ko jodte jao
+#     i += 1          # i ko badhao taaki loop aage chale
+
+# print("Sum is:", total_sum) # Output: 10
+
+
+# ====>
+# task 6
+
+# n = 5
+# factorial = 1 
+
+# while n > 0:
+#     factorial *= n 
+#     n -= 1          
+
+# print("Factorial is:", factorial) # Output: 120
+
+
+
+# ====>
+# task 7
+
+# x = 7
+
+# if x % 2 != 0:
+#     print("odd")
+# else:
+#     print("even")
+
+# ===> 
+# task 8
+# a= input("enter a number ")
+# score = int(a)
+
+# if score >= 90:
+#     print("A")
+# elif score >= 80:
+#     print("B")
+# elif score >= 70:
+#     print("C")
+# else:
+#     print("F")
+
+# ===>
+# task 9
+# no no no no no 
+# a=input("enter a number ")
+# year=int(a)
+# year = 2024
+
+# if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+#     print(True)
+# else:
+#     print(False)
+
+
+# ====>
+# task 10
+# a= input("enter a number between 1 to 7==>")
+# d=int(a)
+# match d:
+#     case 1:
+#         print("Mon")
+#     case 2:
+#         print("Tue")
+#     case 3:
+#         print("Wed")
+#     case 4:
+#         print("Thu")
+#     case 5:
+#         print("Fri")
+#     case _:
+#         print("weekend")
+
+
+
+
+# ====>
+# task 11
+# a=input("enter a number between 100 to 599===>")
+# code=int(a)
+
+# match code // 100:
+#     case 2:
+#         print("ok")
+#     case 4:
+#         print("client error")
+#     case 5:
+#         print("server error")
+#     case _:
+#         print("other")
+
+
+# ====>
+# task 12
+# a=input("enter a password________")
+# cmd= (a)
+
+# match cmd:
+#     case 'quit':
+#         print('exiting')
+#     case 'QUIT':
+#         print('exiting')
+#     case 'help':
+#         print('help text')
+#     case 'HELP':
+#         print('help text')
+#     case _:
+#         print('unknown')
+
+
+# ====>
+# task 12
+# a=input("enter a string==>")
+# print(a[::-1])
+
+# ====>
+# no no no no no no 
+# task 13
+# a=input("enter a string==>")
+# count =0
+# for i in a:
+#     if i in "AEIOUaioeu":
+#         count += 1
+#         print(i)
+#         print("total count of vowels is", count)
+
+
+# ====>
+# task 14
+#  no no no no 
+
+# word="radar"
+# print(word[::-1])
+
+# word="MOM"
+# print(word[::-1])
+
+# word="NITIN"
+# print(word[::-1])
+
+
+# ====>
+# task 14
+
+# name=input("enter your name: ")
+# age=input("enter your age: ")
+# print(f"my name is __ {name} __ and i am __ {age} __ years old")
+
+
+# ====>
+# task 15
+# nums = [1, 2]       
+# nums.append(3)     
+# total = sum(nums)   # sum() function saare elements ko jod deta hai (1+2+3)
+# print(total)
+
+
+
+# ====>
+# task 16
+# values = [4, 9, 2]
+# largest = values[0]   
+
+# for x in values:      
+#     if x > largest:   
+#         largest = x   
+
+# print(largest) 
+
+
+
+# ====>
+# task 17
+# items = [1, 2, 1, 3, 2]
+# unique_list = []      
+
+# for x in items:
+#     if x not in unique_list:  
+#         unique_list.append(x) 
+
+# print(unique_list)  
+
+
+# ====>
+# task 18
+# words = ['a', 'b', 'c']
+
+# for i, w in enumerate(words):
+#     print(i, w)
+
+
+# ====>
+# task 19
+# point = (3,4)
+# print (len(point))
+
+# ====>
+# task 20
+# a = 1
+# b = 2
+# a,b = b,a
+# print(a,b)
+
+
+# ====>
+# task 20
+# s = "hello"
+# print(set(s))
+
+
+# ====>
+# task 21
+# a={1,2,3}
+# b={3,4}
+# print(a|b)
+
+# ====>
+# task 22
+# a=[1,2,3]
+# b=[3,4,5]
+# print(set(a) & set(b))
+
+
+
+
