a1090137e6247c2f3b8058d7fe3dfdbdc763a9fa
Sahil kumawat <viratkumawat137@gmail.com>
2026-05-19 21:05:41 +0530

    Add files via upload

---
+++ filehandling.py
@@ -0,0 +1,27 @@
+# a=open("C:\Users\Dell\OneDrive\Pictures\Screenshots\data scince\python\formateed_string.py")
+# a=open("for_loop.py")
+# print(type(a))
+
+# a=open("for_loop.py")
+# print(dir(a))
+
+
+# a=open("for_loop.py")
+# print(a.readable()) 
+# print(a.read()) 
+
+
+
+
+# a=open("for_loop.py")
+# dat=a.read() 
+# print(dat.upper)
+
+
+# a=open("for_loop.py")
+# b=a.readline()
+# c=a.readline()
+# d=a.readline()
+# print(b,c,d)
+
+# print(a.readlines())
