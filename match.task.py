b72d11dae1cae5d70846391b3c58a74848c0c12f
Sahil kumawat <viratkumawat137@gmail.com>
2026-08-16 17:44:01 +0530

    Update match.task.py

--- match.task.py
+++ match.task.py
@@ -1,7 +1,7 @@
 a=int(input("some word write"))
 b=int(input("some word write")) 
 c=input("which oprater")
-if print(a+b):
+if print(a+b):                             
 	print("check now")
 if print(a-b):
 	print("check now")
